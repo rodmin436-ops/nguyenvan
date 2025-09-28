0e324b5319268c6f4bb9f3287d979a25762909a6
rodmin436-ops <rodmin436@gmail.com>
2025-09-28 20:16:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
   };
 })(jQuery);
 
-// Apply input filter to search fields
+// Apply input filter and focus/blur effects (Chỉ chạy sau khi DOM đã sẵn sàng)
 $(document).ready(function () {
   $("#s1 input").inputFilter(function (value) {
     return /^-?\d*$/.test(value);
@@ -29,38 +29,13 @@ $(document).ready(function () {
     return /^-?\d*$/.test(value);
   });
 
-  // Modal functionality
-  $(".tbc2 .cls, .tbc2 .cls2").click(function () {
-    $(".tbc2").removeClass("tbc2c");
-    $(".ovltbc2").removeClass("tbc2c");
-    $(".qh_new").addClass("bb22");
-    $(".ovl_checkmes").addClass("ovl_checkmesc");
-  });
-
-  $(".qh_new p i").click(function (event) {
-    event.preventDefault();
-    $(".qh_new").removeClass("bb22");
-    $(".ovl_checkmes").removeClass("ovl_checkmesc");
-  });
-
-  // Search field focus effects
-  if ($(window).width() < 768) {
-    $("#s1 input.search-field")
-      .blur(function () {
-        $("#s1 input.search-field").css("border", "1px solid #ddd");
-      })
-      .focus(function () {
-        $(this).css("border", "1px solid blue");
-      });
-  } else {
-    $("#s1 input.search-field")
-      .blur(function () {
-        $("#s1 input.search-field").css("border", "1px solid #fff");
-      })
-      .focus(function () {
-        $(this).css("border", "1px solid blue");
-      });
-  }
+  $("#s1 input.search-field")
+    .blur(function () {
+      $("#s1 input.search-field").css("border", "1px solid #fff");
+    })
+    .focus(function () {
+      $(this).css("border", "1px solid blue");
+    });
 
   $("#mn input.search-field")
     .blur(function () {
@@ -69,12 +44,6 @@ $(document).ready(function () {
     .focus(function () {
       $(this).css("border", "1px solid blue");
     });
-
-  // Set logo in navigation
-  var gtt = $(".gtt").data("ss");
-  var link = "https://admin.checkscam.vn";
-  $("#mn .l").html(gtt);
-  $("#mn .l").parent().attr("href", link);
 });
 
 // AJAX search function
@@ -95,23 +64,7 @@ $(".ess").click(function () {
   $(".ifr").css("display", "none");
 });
 
-// Redirect to HTTPS if on HTTP
-if (window.location.protocol == "http:") {
-  window.location.href = window.location.href.replace("http:", "https:");
+// Redirect to HTTPS if on HTTP (Lưu ý: Dòng này có thể không cần thiết nếu server đã cấu hình sẵn)
+if (window.location.protocol == 'http:') { 
+  window.location.href =  window.location.href.replace( 'http:', 'https:'); 
 }
-
-// Redirect if URL contains 'profile'
-if (window.location.href.indexOf("profile") > 0) {
-  window.location.href = window.location.href.replace("/profile/", "/");
-}
-
-// Redirect if URL contains '/www.'
-if (window.location.href.indexOf("/www.") > 0) {
-  window.location.href = window.location.href.replace("/www.", "/");
-}
-// Thêm đoạn code này để hiển thị thông báo khi trang web tải xong
-$(document).ready(function() {
-    // Thêm class 'tbc2c' để hiển thị thông báo (.tbc2) và lớp phủ (.ovltbc2)
-    $('.tbc2').addClass('tbc2c');
-    $('.ovltbc2').addClass('tbc2c');
-});
